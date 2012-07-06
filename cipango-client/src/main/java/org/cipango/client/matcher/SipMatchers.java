6c42cfb18e7095d85462b728440e965a262d176e
Nicolas <nicolas@cipango.org>
2012-07-06 11:43:57 +0200

    Add matcher on method

--- cipango-client/src/main/java/org/cipango/client/matcher/SipMatchers.java
+++ cipango-client/src/main/java/org/cipango/client/matcher/SipMatchers.java
@@ -1,7 +1,9 @@
 package org.cipango.client.matcher;
 
+import javax.servlet.sip.SipServletMessage;
 import javax.servlet.sip.SipServletResponse;
 
+import org.cipango.client.SipMethods;
 import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
 
@@ -18,5 +20,35 @@ public class SipMatchers
 	public static <T> Matcher<SipServletResponse> hasStatus(int status)
 	{
 		return new HasStatus(status);
+	}
+	
+	@Factory
+	public static <T> Matcher<SipServletMessage> hasMethod(String method)
+	{
+		return new HasMethod(method);
+	}
+	
+	@Factory
+	public static <T> Matcher<SipServletMessage> isInvite()
+	{
+		return new HasMethod(SipMethods.INVITE);
+	}
+	
+	@Factory
+	public static <T> Matcher<SipServletMessage> isBye()
+	{
+		return new HasMethod(SipMethods.BYE);
+	}
+	
+	@Factory
+	public static <T> Matcher<SipServletMessage> isAck()
+	{
+		return new HasMethod(SipMethods.ACK);
+	}
+	
+	@Factory
+	public static <T> Matcher<SipServletMessage> isCancel()
+	{
+		return new HasMethod(SipMethods.CANCEL);
 	}
 }

0759c52384c073328339647c4071b11fd498292c
Nicolas <nicolas@cipango.org>
2012-09-07 08:43:04 +0200

    Cipango-console: Improve about page

--- cipango-console/src/main/java/org/cipango/console/EnvManager.java
+++ cipango-console/src/main/java/org/cipango/console/EnvManager.java
@@ -1,113 +1,179 @@
 package org.cipango.console;
 
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.RuntimeMXBean;
+import java.lang.management.ThreadMXBean;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.Map.Entry;
-import java.util.Properties;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanException;
+import javax.management.MBeanInfo;
 import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
 
 import org.cipango.console.data.Property;
 import org.cipango.console.data.PropertyList;
+import org.cipango.console.util.ObjectNameFactory;
 import org.cipango.console.util.PrinterUtil;
 
 public class EnvManager
 {
 	private MBeanServerConnection _mbsc;
+	private NumberFormat _percentFormat;
 	
+	private static final ObjectName OPERATING_SYSTEM = ObjectNameFactory.create(ManagementFactory.OPERATING_SYSTEM_MXBEAN_NAME);
+		
 	public EnvManager(MBeanServerConnection mbsc)
 	{
 		_mbsc = mbsc;
+		_percentFormat = DecimalFormat.getPercentInstance();
+		_percentFormat.setMinimumFractionDigits(1);
 	}
 	
 	
 	public PropertyList getVersion() throws Exception
 	{
 		PropertyList properties = new PropertyList(_mbsc, JettyManager.SERVER, "version");
-		Long startupTime = (Long) _mbsc.getAttribute(JettyManager.SERVER, "startupTime");
-		properties.add(new Property("Startup Time", new Date(startupTime)));
-		properties.add(new Property("Server Uptime", PrinterUtil.getDuration(System.currentTimeMillis() - startupTime)));
+		properties.add(new Property("Startup Time", new Date(getRuntime().getStartTime())));
+		properties.add(new Property("Server Uptime", PrinterUtil.getDuration(getRuntime().getUptime())));
 		return properties;
 	}
 
-	public PropertyList getJava()
+	public PropertyList getJava() throws Exception
 	{
 		PropertyList env = new PropertyList();
 		env.setTitle("Java");
-		env.add(new Property("Jetty Home", System.getProperty("jetty.home")));
-		env.add(new Property("Java Runtime", System.getProperty("java.runtime.name") + " " + System.getProperty("java.runtime.version")));
+		Map<String, String> properties = getSystemProperties();
+		env.add(new Property("Java Runtime", properties.get("java.runtime.name") + " " + properties.get("java.runtime.version")));
 		
-		Runtime r = Runtime.getRuntime();
-		long usedMemory = r.totalMemory() - r.freeMemory();
-		NumberFormat f = DecimalFormat.getPercentInstance();
-		f.setMinimumFractionDigits(1);
-		String percentage = f.format(((float) usedMemory) / r.maxMemory());
-		env.add(new Property("Memory", (usedMemory >> 20) + " Mb of " + (r.maxMemory() >> 20) + " Mb (" +
+		MemoryUsage r = getMemory().getHeapMemoryUsage();
+		
+		String percentage = _percentFormat.format(((float) r.getUsed()) / r.getMax());
+		env.add(new Property("Memory", (r.getUsed() >> 20) + " Mb of " + (r.getMax() >> 20) + " Mb (" +
 				percentage + ")"));
 
+		env.add(new Property("Threads", getThread().getThreadCount()));
+
+		env.add(new Property("Process CPU time", 
+				PrinterUtil.getDuration((Long) _mbsc.getAttribute(OPERATING_SYSTEM, "ProcessCpuTime")/1000000)));
+		
+		String load = isLoadAvailable() ? _percentFormat.format((Double) _mbsc.getAttribute(OPERATING_SYSTEM, "ProcessCpuLoad"))
+				: "Not available";
+		env.add(new Property("Process CPU load", load));
+		
 		StringBuilder sb = new StringBuilder();
 		for (String arg : ManagementFactory.getRuntimeMXBean().getInputArguments())
 			sb.append(arg).append(' ');
 		env.add(new Property("VM arguments", sb.toString()));
+		env.add(new Property("Jetty Home", properties.get("jetty.home")));
 		
 		return env;
 	}
+	
+	
 
-	public PropertyList getHardware()
+	public PropertyList getHardware() throws Exception
 	{
 		PropertyList env = new PropertyList();
 		env.setTitle("Hardware");
-		env.add(new Property("OS / Hardware", System.getProperty("os.name") + " " + System.getProperty("os.version")
-				+ " - " + System.getProperty("os.arch")));
+		Map<String, String> properties = getSystemProperties();
 		
-		NumberFormat f = DecimalFormat.getPercentInstance();
-		f.setMinimumFractionDigits(1);
-		
-		OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
-		
-		double loadAvg = osBean.getSystemLoadAverage();
-		env.add(new Property("System load average", loadAvg < 0 ? "Not available" : loadAvg));
-		if (osBean instanceof com.sun.management.OperatingSystemMXBean)
+		env.add(new Property("OS / Hardware", properties.get("os.name") + " " + properties.get("os.version")
+				+ " - " + properties.get("os.arch")));
+				
+		OperatingSystemMXBean os = getOperatingSystem();
+				
+		String load = isLoadAvailable() ? _percentFormat.format((Double) _mbsc.getAttribute(OPERATING_SYSTEM, "SystemCpuLoad"))
+				: "Not available";
+		env.add(new Property("System CPU load", load));
+
+		if (isHardwareMemoryAvailable())
 		{
-			com.sun.management.OperatingSystemMXBean osBean2 = (com.sun.management.OperatingSystemMXBean) osBean;
-			long totalMemory = osBean2.getTotalPhysicalMemorySize();
-			long usedMemory = totalMemory - osBean2.getFreePhysicalMemorySize();
-			String percentage = f.format(((float) usedMemory) / totalMemory);
+			long totalMemory = (Long) _mbsc.getAttribute(OPERATING_SYSTEM, "TotalPhysicalMemorySize");
+			long freeMemory = (Long) _mbsc.getAttribute(OPERATING_SYSTEM, "FreePhysicalMemorySize");
+			long usedMemory = totalMemory - freeMemory;
+			String percentage = _percentFormat.format(((float) usedMemory) / totalMemory);
 			env.add(new Property("Physical memory", (usedMemory >> 20) + " Mb used of "
 					+ (totalMemory >> 20) + " Mb (" + percentage + ")"));
 			
-			totalMemory = osBean2.getTotalSwapSpaceSize();
-			usedMemory = totalMemory - osBean2.getFreeSwapSpaceSize();
-			percentage = f.format(((float) usedMemory) / totalMemory);
+			totalMemory = (Long) _mbsc.getAttribute(OPERATING_SYSTEM, "TotalSwapSpaceSize");
+			freeMemory = (Long) _mbsc.getAttribute(OPERATING_SYSTEM, "FreeSwapSpaceSize");
+			usedMemory = totalMemory - freeMemory;
+			percentage = _percentFormat.format(((float) usedMemory) / totalMemory);
 			env.add(new Property("Swap memory", (usedMemory >> 20) + " Mb used of "
 					+ (totalMemory >> 20) + " Mb (" + percentage + ")"));
 		}
 		
-		env.add(new Property("Available processors", osBean.getAvailableProcessors()));
+		env.add(new Property("Available processors", os.getAvailableProcessors()));
 		StringBuilder sb = new StringBuilder();
-		for (String arg : ManagementFactory.getRuntimeMXBean().getInputArguments())
+		for (String arg :  getRuntime().getInputArguments())
 			sb.append(arg).append(' ');
 		
 		return env;
 	}
 	
-	public SortedMap<String, String> getSystemProperties()
+	private boolean isLoadAvailable() throws Exception
 	{
-		Properties properties = System.getProperties();
+		if (!_mbsc.isRegistered(OPERATING_SYSTEM))
+			return false;
 		
-		SortedMap<String, String> map = new TreeMap<String, String>();
-		Iterator<Entry<Object, Object>> it = properties.entrySet().iterator();
-		while (it.hasNext())
-		{
-			Entry<Object, Object> entry = it.next();
-			map.put((String) entry.getKey(), (String) entry.getValue());	
-		}
+		MBeanInfo info = _mbsc.getMBeanInfo(OPERATING_SYSTEM);
+		for (MBeanAttributeInfo attrInfo : info.getAttributes())
+			if ("ProcessCpuLoad".equals(attrInfo.getName()))
+				return true;
+		return false;
+	}
+	
+	private boolean isHardwareMemoryAvailable() throws Exception
+	{
+		if (!_mbsc.isRegistered(OPERATING_SYSTEM))
+			return false;
+		
+		MBeanInfo info = _mbsc.getMBeanInfo(OPERATING_SYSTEM);
+		for (MBeanAttributeInfo attrInfo : info.getAttributes())
+			if ("TotalPhysicalMemorySize".equals(attrInfo.getName()))
+				return true;
+		return false;
+	}
+	
+	public OperatingSystemMXBean getOperatingSystem() throws IOException
+	{
+		return ManagementFactory.newPlatformMXBeanProxy(_mbsc, ManagementFactory.OPERATING_SYSTEM_MXBEAN_NAME, OperatingSystemMXBean.class);
+	}
+	
+	
+	public RuntimeMXBean getRuntime() throws IOException
+	{
+		return ManagementFactory.newPlatformMXBeanProxy(_mbsc, ManagementFactory.RUNTIME_MXBEAN_NAME, RuntimeMXBean.class);
+	}
+	
+	public MemoryMXBean getMemory() throws IOException
+	{
+		return ManagementFactory.newPlatformMXBeanProxy(_mbsc, ManagementFactory.MEMORY_MXBEAN_NAME, MemoryMXBean.class);
+	}
+	
+	public ThreadMXBean getThread() throws IOException
+	{
+		return ManagementFactory.newPlatformMXBeanProxy(_mbsc, ManagementFactory.THREAD_MXBEAN_NAME, ThreadMXBean.class);
+	}
+	
+	public SortedMap<String, String> getSystemProperties() throws AttributeNotFoundException, InstanceNotFoundException, MBeanException, ReflectionException, IOException
+	{	
+		Map<String, String> properties = getRuntime().getSystemProperties();
+		
+		SortedMap<String, String> map = new TreeMap<String, String>(properties);
 		return map;
 	}
 }

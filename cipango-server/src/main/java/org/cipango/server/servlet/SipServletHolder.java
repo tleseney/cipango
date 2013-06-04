fd0ac69aea83d9f70908e6f2896925430fca0d5e
Nicolas <nicolas@cipango.org>
2013-06-04 09:47:57 +0200

    Add missing method that creates a StackOverflowError when a Sip App failed to deploy
    Signed-off-by: Nicolas <nicolas@cipango.org>

--- cipango-server/src/main/java/org/cipango/server/servlet/SipServletHolder.java
+++ cipango-server/src/main/java/org/cipango/server/servlet/SipServletHolder.java
@@ -229,6 +229,26 @@ public class SipServletHolder extends Holder<SipServlet> implements UserIdentity
         }
 	}
 	
+    private void makeUnavailable(UnavailableException e)
+    {
+        if (_unavailableEx==e && _unavailable!=0)
+            return;
+
+        _servletHandler.getServletContext().log("unavailable",e);
+
+        _unavailableEx=e;
+        _unavailable=-1;
+        if (e.isPermanent())
+            _unavailable=-1;
+        else
+        {
+            if (_unavailableEx.getUnavailableSeconds()>0)
+                _unavailable=System.currentTimeMillis()+1000*_unavailableEx.getUnavailableSeconds();
+            else
+                _unavailable=System.currentTimeMillis()+5000; // TODO configure
+        }
+    }
+	
     @SuppressWarnings("serial")
 	private void makeUnavailable(final Throwable e)
     {

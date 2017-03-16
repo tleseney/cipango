97f95809c19a30f1bec472a515b52f66d6fed9c2
Thomas Leseney <leseney@gmail.com>
2017-03-16 09:25:37 +0100

    started headers

---
+++ src/main/java/org/cipango/server/SipMessage.java
@@ -0,0 +1,7 @@
+package org.cipango.server;
+
+/**
+ * Created by thomas on 08/03/2017.
+ */
+public class SipMessage {
+}
---
+++ src/main/java/org/cipango/sip/SipHeader.java
@@ -0,0 +1,14 @@
+package org.cipango.sip;
+
+public enum SipHeader {
+
+    CALL_ID("Call-ID");
+
+    private String name;
+
+    SipHeader(String name) {
+        this.name = name;
+
+    }
+
+}
